659ead6aa9bb026dbbc082bb658a15ac7b8b9bb9
Qwertymon <33938081+Qwertymon@users.noreply.github.com>
2018-04-09 22:04:54 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,33 @@
+import socket
 from flask import Flask, render_template
-from flask_socketio import SocketIO, send, emit, join_room
-import random
+import threading
 
 
 
-chat_room_id = []
+
+
+def Socket():
+	host = socket.gethostbyname(socket.gethostname())
+	s =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.bind((host,8080))
+	s.listen(10)
+	print "Server start"
+	while True:
+		con, addr = s.accept()
+		con.send("hi")
+		con.close()
+
+l = threading.Thread(target = Socket)
+l.start()
 
 
 app = Flask(__name__)
-app.config['SECRET-KEY'] = 'scret'
-socketio = SocketIO(app)
-
 @app.route('/')
 def index():
-        return render_template('main.html')
+	host = socket.gethostbyname(socket.gethostname())
+	return render_template('main.html', title = host)
 
-@app.route('/chatroom/<roomid>')
-def chat(roomid):
-        global chat_room_id
-        if roomid not in chat_room_id:
-                return "<h1>No such room</h1>"
-        else:
-                return render_template('chat.html')
-
-
-        
-@socketio.on('message')
-def handle_maessage(msg):
-        global chat_room_id
-        if msg == "room":
-                letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-                key = ''
-                for i in range(0,8):
-                        key += random.choice(letters)
-                chat_room_id.append(key)
-                emit("key",key)
-        else:
-                room = msg[-9:-1]
-                print room
-                emit(room,msg[0:-9], broadcast = True)
-
-if __name__ == '__main__':
-        socketio.run(app)
+host = socket.gethostbyname(socket.gethostname())
+print host
+if __name__ == "__main__":
+	app.run(port = 8080)
