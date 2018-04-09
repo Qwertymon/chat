c6c22aafb7d8cafbf623ef90d9b4716e523393f4
Qwertymon <33938081+Qwertymon@users.noreply.github.com>
2018-04-09 22:12:25 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,32 @@
 import socket
 from flask import Flask, render_template
 import threading
-
+import time
 
 
 
 
 def Socket():
-	host = socket.gethostbyname(socket.gethostname())
-	s =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.bind((host,8080))
-	s.listen(10)
-	print "Server start"
-	while True:
-		con, addr = s.accept()
-		con.send("hi")
-		con.close()
+              global host
+              host = socket.gethostbyname(socket.gethostname())
+              s =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+              s.bind((host,8080))
+              s.listen(10)
+              while True:
+                            con, addr = s.accept()
+                            con.send("hi")
+                            con.close()
 
 l = threading.Thread(target = Socket)
 l.start()
-
-
+host = ''
+time.sleep(0.5)
 app = Flask(__name__)
 @app.route('/')
 def index():
-	host = socket.gethostbyname(socket.gethostname())
-	return render_template('main.html', title = host)
+              global host
+              return render_template('main.html', title = host)
 
-host = socket.gethostbyname(socket.gethostname())
-print host
 if __name__ == "__main__":
 	app.run(port = 8080)
+
