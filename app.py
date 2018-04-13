1a299e68aa324f8595f8297a4198fbde23b42647
Qwertymon <33938081+Qwertymon@users.noreply.github.com>
2018-04-13 18:06:41 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,19 @@
 import socket
-from flask import Flask, render_template
-import threading
-import time
 
 
+s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = socket.gethostbyname(socket.gethostname())
+s.bind(("0.0.0.0",8080))                                               
+s.listen(10)                                            
+f = open("log.txt","w")
+f.write(str(host)+" listeniing")
+f.close()
+while True:
+              con, addr = s.accept()
+              f1 = open("clients.txt","w")
+              f1.write(str(addr))
+              f1.close()
+              con.send("LOL")
+              con.close()
 
-
-def Socket():
-              global host
-              host = socket.gethostbyname(socket.gethostname())
-              s =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-              s.bind((host,8080))
-              s.listen(10)
-              while True:
-                            con, addr = s.accept()
-                            con.send("hi")
-                            con.close()
-
-l = threading.Thread(target = Socket)
-l.start()
-host = ''
-time.sleep(0.5)
-app = Flask(__name__)
-@app.route('/')
-def index():
-              global host
-              return render_template('main.html', title = host)
-
-if __name__ == "__main__":
-	app.run(port = 8080)
-
+s.close()
