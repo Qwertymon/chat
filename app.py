cb741e871b62660256138227ce7a4b843ee96e74
Qwertymon <33938081+Qwertymon@users.noreply.github.com>
2018-04-17 13:55:16 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,37 @@
 import socket
+from flask import Flask
+import  threading 
 
-host = socket.gethostbyname(socket.gethostname())
-port = 8080
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((host,port))
-s.listen(10)
-f = open("file.txt",'w')
-f.write("created "+str(host))
-f.close()
 
-s.close()
+globalhost = ''
+gloabalk = "flase"
+
+
+def sockett():
+        global globalhost, gloabalk
+        
+        host = socket.gethostbyname(socket.gethostname())
+        globalhost = host
+        port = 8080
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((host,port))
+        s.listen(10)
+        gloabalk = "true"
+        while True:
+                con, addr = s.accept()
+                con.send("Nope")
+                con.close()
+        s.close()
+
+t = threading.Thread(target = sockett)
+t.start()
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+        global globalhost, gloabalk
+        return  str(globalhost)+" "+str(gloabalk)
+
+if  __name__ == "__main__":
+        app.run()
