a105d88490acb1c86393465921dfe671c0441c22
Qwertymon <33938081+Qwertymon@users.noreply.github.com>
2018-04-17 14:30:21 +0400

    Update app.py

--- app.py
+++ app.py
@@ -8,30 +8,12 @@ globalhost = ''
 gloabalk = "flase"
 
 
-def sockett():
-        global globalhost, gloabalk
-        
-        host = socket.gethostbyname(socket.gethostname())
-        globalhost = host
-        port = 8080
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind((host,port))
-        s.listen(10)
-        gloabalk = "true"
-        while True:
-                con, addr = s.accept()
-                con.send("Nope")
-                con.close()
-        s.close()
-
-t = threading.Thread(target = sockett)
-t.start()
 
 app = Flask(__name__)
 @app.route('/')
 def index():
         global globalhost, gloabalk
-        return  str(globalhost)+" "+str(gloabalk)
+        return  "sdf"
 
 if  __name__ == "__main__":
         app.run()
