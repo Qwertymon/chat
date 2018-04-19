e38295676d5f7698c983709ac12282ff2607f0d0
Qwertymon <33938081+Qwertymon@users.noreply.github.com>
2018-04-19 13:48:52 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask
+from datetime import datetime
 app = Flask(__name__)
-@app.route('/')
-def index():
-    return render_template('main.html', title = "nothing")
 
-if  __name__ == "__main__":
-    app.run(port = 8080)
+@app.route('/')
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+
+    return """
+    <h1>Hello heroku</h1>
+    <p>It is currently {time}.</p>
+    <img src="http://loremflickr.com/600/400" />
+    """.format(time=the_time)
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
