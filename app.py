a047d1cb94f87e8fc1be3e5fb0d0b7f1e39a18b6
Qwertymon <33938081+Qwertymon@users.noreply.github.com>
2018-04-20 15:54:41 +0400

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template
 import socket
 
 host = socket.gethostbyname(socket.gethostname())
+
 app = Flask(__name__)
+
 @app.route('/')
 def index():
     global host
     return render_template('main.html', title = host)
 
 if  __name__ == "__main__":
-    app.run(debug=True, use_reloader=True)
+    app.run(debug=True)
