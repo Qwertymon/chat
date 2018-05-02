9968f0cac1e1ef740585dff25823001a1d5d57de
Qwertymon <33938081+Qwertymon@users.noreply.github.com>
2018-05-02 21:13:16 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
-from flask import Flask, render_template
 import socket
+import threading
+from flask import Flask
 
-host = socket.gethostbyname(socket.gethostname())
+
+host = 'myownserver.herokuapp.com'
+host_ip = ''
+chk_server = "false"
+
+def server():
+    global host, host_ip, chk_server
+    host_ip = socket.gethostbyname(host)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host_ip,8080))
+    s.listen(10)
+    chk_server = "True"
+    while True:
+        con, addr = s.accept()
+        con.send("good")
+        con.close()
+
+t = threading.Thread(target = server)
+t.start()
 
 app = Flask(__name__)
-
 @app.route('/')
 def index():
-    global host
-    return render_template('main.html', title = host)
+    global  host_ip, chk_server
+    return str(host_ip)+" "+str(chk_server)
 
-if  __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
