2e8b71110af04f21c9e7e4778c1ae67a7102ee7e
Qwertymon <33938081+Qwertymon@users.noreply.github.com>
2018-05-24 14:58:43 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,18 @@
 import socket
-import threading
 from flask import Flask
 
 
 
 host_ip = ''
-chk_server = "false"
 
-def server():
-    global host_ip, chk_server
-    host_ip = socket.gethostbyname(socket.gethostname())
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((host_ip,8080))
-    s.listen(10)
-    chk_server = "True"
-    while True:
-        con, addr = s.accept()
-        con.send("good")
-        con.close()
 
-t = threading.Thread(target = server)
-t.start()
+host_ip = socket.gethostbyname(socket.gethostname())
 
 app = Flask(__name__)
 @app.route('/')
 def index():
-    global  host_ip, chk_server
-    return str(host_ip)+" "+str(chk_server)
+    global  host_ip
+    return str(host_ip)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host = host_ip, port = 8080)
